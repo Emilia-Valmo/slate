24c146b96bdbac857b46361e1364795babd2ab9e
Samy Pessé <samypesse@gmail.com>
2019-03-14 00:53:20 +0100

    Adapt tests for slate-edit-list

--- packages/slate-edit-list/test/index.test.ts
+++ packages/slate-edit-list/test/index.test.ts
@@ -1,60 +1,62 @@
 /* eslint-disable import/no-dynamic-require */
 /* eslint-disable global-require */
 
-import fs from 'fs'
-import path from 'path'
-import Slate from '@gitbook/slate'
-import hyperprint from '@gitbook/slate-hyperprint'
+import Slate from '@gitbook/slate';
+import hyperprint from '@gitbook/slate-hyperprint';
+import fs from 'fs';
+import path from 'path';
 
-import EditList from '../src'
+import EditList from '../src';
 
 // Provide the value with
 function deserializeValue(plugin, value) {
-  const SCHEMA = Slate.Schema.create({
-    plugins: [plugin],
-  })
+    const SCHEMA = Slate.Schema.create({
+        plugins: [plugin]
+    });
 
-  return Slate.Value.fromJS(
-    {
-      selection: value.selection,
-      document: value.document,
-      schema: SCHEMA,
-    },
-    { normalize: false }
-  )
+    return Slate.Value.fromJS(
+        {
+            selection: value.selection,
+            document: value.document,
+            schema: SCHEMA
+        },
+        { normalize: false }
+    );
 }
 
 describe('slate-edit-list', () => {
-  const tests = fs.readdirSync(__dirname)
+    const tests = fs.readdirSync(__dirname);
 
-  tests.forEach((test, index) => {
-    if (test[0] === '.' || path.extname(test).length > 0) return
+    tests.forEach((test, index) => {
+        if (test[0] === '.' || path.extname(test).length > 0) {
+            return;
+        }
 
-    it(test, () => {
-      const dir = path.resolve(__dirname, test)
-      const plugin = EditList()
+        it(test, () => {
+            const dir = path.resolve(__dirname, test);
+            const plugin = EditList();
 
-      const input = deserializeValue(
-        plugin,
-        require(path.resolve(dir, 'input.js')).default
-      )
+            const input = deserializeValue(
+                plugin,
+                require(path.resolve(dir, 'input')).default
+            );
 
-      const expectedPath = path.resolve(dir, 'expected.js')
-      const expected =
-        fs.existsSync(expectedPath) &&
-        deserializeValue(plugin, require(expectedPath).default)
+            const expectedPath = path.resolve(dir, 'expected');
+            const expected =
+                fs.existsSync(expectedPath) &&
+                deserializeValue(plugin, require(expectedPath).default);
 
-      const runChange = require(path.resolve(dir, 'change.js')).default
+            const runChange = require(path.resolve(dir, 'change')).default;
 
-      const newChange = runChange(plugin, input.change())
+            const newChange = runChange(plugin, input.change());
 
-      if (expected) {
-        const actual = newChange.value
+            if (expected) {
+                const actual = newChange.value;
 
-        expect(hyperprint(actual, { strict: true })).toEqual(
-          hyperprint(expected, { strict: true })
-        )
-      }
-    })
-  })
-})
+                expect(hyperprint(actual, { strict: true })).toEqual(
+                    hyperprint(expected, { strict: true })
+                );
+            }
+        });
+    });
+});

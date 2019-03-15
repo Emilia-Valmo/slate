e9f6bcc296c8fd8c0c27c6c122ebe938b63e92e9
Samy Pessé <samypesse@gmail.com>
2019-03-15 12:27:53 +0100

    Fix Change.setValue

--- packages/slate/src/changes/on-value.ts
+++ packages/slate/src/changes/on-value.ts
@@ -1,39 +1,28 @@
 import Value from '../models/value';
+import Operation from '../models/operation';
 
 /*
  * Changes.
- *
- * @type {Object}
  */
 
 const Changes = {};
 
 /*
  * Set `properties` on the value.
- *
- * @param {Change} change
- * @param {Object|Value} properties
- * @param {Object} options
  */
-
 Changes.setValue = (change, properties, options = {}) => {
     properties = Value.createProperties(properties);
     const { value } = change;
 
     change.applyOperation(
-        {
+        Operation.create({
             type: 'set_value',
             properties,
             value
-        },
+        }),
         options
     );
 };
 
-/*
- * Export.
- *
- * @type {Object}
- */
 
 export default Changes;
